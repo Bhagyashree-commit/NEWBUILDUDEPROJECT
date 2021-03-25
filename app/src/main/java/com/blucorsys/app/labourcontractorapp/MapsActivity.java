4ca53e0cae1d6a5f05631800e046db72af339109
TechyRashmi <dwivedirashmi321@gmail.com>
2021-03-25 14:26:01 +0530

    Rashmi map

--- app/src/main/java/com/blucorsys/app/labourcontractorapp/MapsActivity.java
+++ app/src/main/java/com/blucorsys/app/labourcontractorapp/MapsActivity.java
@@ -88,7 +88,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
         View locationButton = ((View)findViewById(Integer.parseInt("1")).getParent()).findViewById(Integer.parseInt("2"));
         RelativeLayout.LayoutParams rlp = (RelativeLayout.LayoutParams) locationButton.getLayoutParams();
-// position on right bottom
+        // position on right bottom
         rlp.addRule(RelativeLayout.ALIGN_PARENT_TOP, 0);
         rlp.addRule(RelativeLayout.ALIGN_PARENT_TOP, RelativeLayout.TRUE);
         rlp.setMargins(0, 180, 180, 0);
